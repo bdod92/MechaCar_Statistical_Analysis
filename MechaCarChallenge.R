e2da08f95adce6c721afb5c2710b1a570fa73838
Brayden Dodson <braydendodson@gmail.com>
2022-06-05 16:58:38 -0700

    Stat analyses of suspension variability

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,15 @@
+#include dplyr library
 library(dplyr)
-mechaCarDF <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCarDF))
+
+#create mpg DF
+mechaCarMPGDF <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+#generate linear model AND summary of linear model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCarMPGDF))
+
+#create a suspension DF
+mechaCarSusDF <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+#generate statistics for PSI
+total_summary <- mechaCarSusDF%>% summarize(Mean = mean(mechaCarSusDF$PSI), Median = median(mechaCarSusDF$PSI), Variance = var(mechaCarSusDF$PSI), SD = sd(mechaCarSusDF$PSI), .groups = 'keep')
+#create a lot summary 
+lot_summary <- mechaCarSusDF %>% group_by(Manufacturing_Lot) %>%summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
